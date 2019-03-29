d51e16324975b97bc5fe821e1cfd59cece9b7084
Reyzaldy Indra <ireyzaldi@gmail.com>
2019-03-29 13:39:25 +0700

    home layout first ver

--- app/src/main/java/com/example/slebewpintar/MainActivity.java
+++ app/src/main/java/com/example/slebewpintar/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.slebewpintar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnPay, btnNearby, btnTopUp, btnStore, btnTourism, btnSouvenir, btnCulinary;
+    TextView balance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        btnPay = findViewById(R.id.btn_pay);
+        btnNearby = findViewById(R.id.btn_nearby);
+        btnCulinary = findViewById(R.id.btn_culinary);
+        btnTopUp = findViewById(R.id.btn_topup);
+        btnStore =  findViewById(R.id.btn_store);
+        btnTourism = findViewById(R.id.btn_tourism);
+        btnSouvenir = findViewById(R.id.btn_souvenirs);
+        balance = findViewById(R.id.tx_balance);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
